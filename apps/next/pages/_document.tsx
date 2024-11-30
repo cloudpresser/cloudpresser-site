181c209eea4a138b7ed174b4c55f53f0f5cbbe8d
Luiz Ozorio <luiz.ozorio@vectorvest.com>
2024-11-30 12:37:03 -0500

    chore(document): add preload link for Montserrat font

--- apps/next/pages/_document.tsx
+++ apps/next/pages/_document.tsx
@@ -9,6 +9,10 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" />
       </Head>
       <body className="bg-white">
         <Main />
