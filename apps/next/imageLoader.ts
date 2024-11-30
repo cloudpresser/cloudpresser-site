0c0e813a9547c0fabeefd357708e27823085b6a5
Luiz Ozorio <luiz.ozorio@vectorvest.com>
2024-11-30 11:49:20 -0500

    feat(imageLoader): add auto format support for images

--- apps/next/imageLoader.ts
+++ apps/next/imageLoader.ts
@@ -10,7 +10,7 @@ export default function cloudflareLoader({
   if (process.env.NODE_ENV === "development") {
     return src;
   }
-  const params = [`width=${width}`];
+  const params = [`width=${width}`, 'format=auto'];
   if (quality) {
     params.push(`quality=${quality}`);
   }
